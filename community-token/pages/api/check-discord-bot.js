5397b63c9af25fa4d126459daac569e0a36e1758
kshitijchakravarty <chakravartykshitij@gmail.com>
2022-07-16 16:23:30 -0400

    added api-endpoints for new-community-form

---
+++ community-token/pages/api/check-discord-bot.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+  /* check to see if discord bot was invited */
+  // send success or error message based on guild count
+}
---
+++ community-token/pages/api/new-community.js
@@ -0,0 +1,31 @@
+import { create } from "ipfs-http-client";
+
+export default function handler(req, res) {
+  /* init */
+  const Moralis = require("moralis/node");
+  const serverUrl = process.env.SERVER_URL;
+  const appId = process.env.APP_ID;
+  const masterKey = process.env.MASTER_KEY;
+
+  const ipfs = create("https://ipfs.infura.io:5001");
+
+  const body = req.body;
+
+  /* upload community logo to ipfs */
+  const upload = await ipfs.add(body.bufferArray);
+  const logoURL = "https://gateway.ipfs.io/ipfs/" + upload.path;
+
+  /* store community data in moralis database */
+  await Moralis.start({ serverUrl, appId, masterKey });
+  const communityClass = Moralis.Object.extend("Communities");
+  const community = new communityClass();
+  community.set("Name", body.name);
+  community.set("Description", body.description);
+  community.set("LogoURL", logoURL);
+  community.set("OwnerAddress", body.ownerAddress);
+  community.set("CloneAddress", body.cloneAddress);
+  await community.save();
+
+  /* send "community created" message to server via bot */
+
+}
