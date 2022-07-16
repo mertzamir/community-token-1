fd1131ceb734103fdb9510217eeada04c2fc3605
Nadirah Durr <31542146+nadirahdurr@users.noreply.github.com>
2022-07-16 17:14:54 -0400

    added submit button

--- community-token/pages/community/createcommunity.js
+++ community-token/pages/community/createcommunity.js
@@ -10,26 +10,43 @@ export default function CreateCommunity() {
       </div>
       <div className={styles.container}>
         <main className={styles.main}>
-          <div className="md:mb-12 mb-8 lg:mb-16 lg:text-[5vh] md:text-[3vh] text-[20px] text-center lg:mt-12 lg:mb-4 md:mt-28 md:mb-[10px] mt-12  text-white">
+          <div className="md:mb-12 mb-8 lg:mb-24 lg:text-[5vh] md:text-[3vh] text-[20px] text-center lg:mt-12 lg:mb-4 md:mt-28 md:mb-[10px] mt-12  text-white">
             Create New Community
           </div>
-          <form className="bg-[#23024d] md:px-24 md:py-24 px-12 py-12 rounded-2xl flex justify-center grid grid-cols-1 gap-4 opacity-75 border-4 border-white">
+          <form className="bg-[#23024d] md:px-24 md:py-12 px-12 py-12 rounded-2xl flex justify-center grid grid-cols-1 gap-4 opacity-75 border-4 border-white">
             <label className="block cursor-pointer text-center text-white">
               Name
             </label>
-            <input className="rounded-2xl  h-12 pl-2 text-center" maxLength="30" type="text"></input>
+            <input
+              className="rounded-2xl  h-12 pl-2 text-center"
+              minlength="1"
+              maxLength="30"
+              type="text"
+              required
+            ></input>
             <label className="block cursor-pointer text-center text-white">
               Short Description
             </label>
-            <textarea className="rounded-2xl h-24 pl-2" maxLength="80" type="text"></textarea>
+            <textarea
+              className="rounded-2xl h-24 pl-2"
+              minlength="10"
+              maxLength="80"
+              type="text"
+              required
+            ></textarea>
             <label className="block cursor-pointer text-center text-white">
               Upload Logo
             </label>
-            <span className="sr-only">Choose File</span>
             <input
+              required
               type="file"
-              className="cursor-pointer block w-full text-sm text-white file:mr-4 file:py-2 md:file:px-12 file:px-2 file:py-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-white file:text-[#23024d] hover:file:bg-gray-100"
+              className="cursor-pointer block w-full text-sm text-white file:mr-4 file:py-2 md:file:px-8 file:px-2 file:py-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-white file:text-[#23024d] hover:file:bg-gray-100"
             />
+            <div className="mt-8">
+              <button type="submit" className="md:px-36 px-16 font-bold py-4 bg-white rounded-full text-[#23024d] hover:bg-gray-100">
+                Submit
+              </button>
+            </div>
           </form>
         </main>
       </div>
